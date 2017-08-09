0d63942634e9d0333761f0cbf1e136f224bae233
Naris Razafimahatratra <nrazafimahatratra@valueit.fr>
2017-08-09 16:00:04 +0300

    set theme to dark

--- components/Home.vue
+++ components/Home.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <v-toolbar flat class="transparent" fixed>
+    <div class="main-section">
+        <v-toolbar dark flat class="transparent" fixed>
             <v-spacer></v-spacer>
             <v-toolbar-side-icon class="hidden-md-and-up"></v-toolbar-side-icon>
             <v-toolbar-items class="hidden-sm-and-down">
@@ -11,7 +11,7 @@
                 <v-btn flat>Contact</v-btn>
             </v-toolbar-items>
         </v-toolbar>
-        <vue-particles color="#dedede"  class="bg" :particleOpacity="1" :particlesNumber="80" shapeType="circle" :particleSize="8"
+        <vue-particles color="#dedede" class="bg" :particleOpacity="1" :particlesNumber="80" shapeType="circle" :particleSize="4"
             linesColor="#dedede" :linesWidth="1" :lineLinked="true" :lineOpacity="0.4" :linesDistance="150" :moveSpeed="3" :hoverEffect="true"
             hoverMode="grab" :clickEffect="true" clickMode="push">
         </vue-particles>
@@ -21,10 +21,37 @@
 
 <style lang="css" scoped>
     .bg {
-        background-image: url("http://newboundnetwork.com/wp-content/uploads/2014/04/icon-feed-bot.png");
+        background-color: black;
         height: 100vh;
         background-position: center;
         background-repeat: no-repeat;
         background-size: cover;
+    }
+
+    .main-title {
+        margin: auto;
+        padding: 0 1rem;
+        color: #fff;
+        z-index: 999;
+        position: relative;
+        top: 600px;
+    }
+
+    .main-title h2 {
+        font-size: 7rem;
+        font-family: 'Teko', sans-serif;
+        text-shadow: 8px 8px #000000;
+        text-transform: uppercase;
+        @media (max-width: 767px) {
+            font-size: 3rem;
+        }
+    }
+
+    .main-title p.subtitle {
+        font-size: 1.7rem;
+        margin: 0;
+        @media (max-width: 767px) {
+            font-size: 1rem;
+        }
     }
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app light toolbar footer>
+  <v-app toolbar footer>
     <nuxt/>
   </v-app>
 </template>
